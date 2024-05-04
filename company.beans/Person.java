f715b83466e3f09e0ade3f853061469ba0138dde
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-05-04 17:41:42 +0200

    Update Person.java

--- company.beans/Person.java
+++ company.beans/Person.java
@@ -1,1 +1,33 @@
+package company.beans;
 
+import compnay.exceptions.*;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person (String name, int age) throws InvalidAgeException {
+        if (age < 0 || age > 125) {
+            throw new InvalidAgeExepction ("Invalid age: " + age);
+        }
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
