75bdd36a3a950a91e69aca6b406c4b96ed2ae3fc
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-05-04 17:52:28 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+import company.beans.*;
+import company.exceptions.*;
 
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            Person person = new Person("Will", 25);
+            System.out.println("Person's name: " + person.getName());
+            System.out.println("Person's age: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
