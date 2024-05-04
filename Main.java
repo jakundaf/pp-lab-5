01c91e1f66bcc909a1349dabf4ec6f0447987797
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-05-04 18:05:52 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import company.beans.*;
 import company.exceptions.*;
+import company.interfaces.*;
+import company.implementations.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +10,10 @@ public class Main {
             Person person = new Person("Will", 25);
             System.out.println("Person's name: " + person.getName());
             System.out.println("Person's age: " + person.getAge());
+
+            EmailMessenger messenger = new EmailMessenger();
+            messenger.sendMessage("Message from the EmailMessenger");
+
         } catch (InvalidAgeException e) {
             System.out.println("Exception: " + e.getMessage());
         }
